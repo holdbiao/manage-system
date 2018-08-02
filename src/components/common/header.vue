ceaabd55ffe3bbc5bc870a26c0ec9324a12b286a
biu <1007059411@qq.com>
2018-08-03 07:42:50 +0800

    小修改

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="header-wrapper">
-    <div class="left-wrapper">
-       <div class="flex-button">
-       <i class="el-icon-menu"></i>
-       </div>
-       <div class="name">后台管理系统</div>
-    </div>
-</div>
+  <div class="header-wrapper">
+      <div class="left-wrapper">
+         <div class="flex-button">
+            <i class="el-icon-menu"></i>
+         </div>
+         <div class="name">后台管理系统</div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -20,30 +20,28 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-$main-color: #242f42;
-.header-wrapper {
-  height: 60px;
-  background-color: $main-color;
-  & .flex-button {
-      float: left;
-      width: 60px;
-      height: 100%;
-      text-align: center;
-      line-height: 60px;
-    & i {
-      font-size: 30px;
-      color: #fff;
-      line-height: 60px;
+  $main-color: #242f42;
+  .header-wrapper {
+    height: 60px;
+    background-color: $main-color;
+    & .flex-button {
+        float: left;
+        width: 60px;
+        text-align: center;
+        line-height: 60px;
+      & i {
+        font-size: 20px;
+        color: #fff;
+        line-height: 60px;
+      }
+    }
+    .name {
+        float: left;
+        height: 60px;
+        line-height: 60px;
+        font-size: 20px;
+        color: #fff;
     }
   }
-  .name {
-      display: inline-block;
-      height: 60px;
-      line-height: 60px;
-      font-size: 20px;
-      color: #fff;
-  }
-}
 </style>
