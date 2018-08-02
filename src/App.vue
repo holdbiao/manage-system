cee326c14a3b57790c5c8812bbe672b8c786d78b
biu <1007059411@qq.com>
2018-08-02 18:58:26 +0800

    'newproject'

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* 引入重设网页css样式 */
+@import '../static/css/reset.css'
+</style>
---
+++ src/components/common/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1> -->
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import vHeader from './header'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your home！'
+    }
+  },
+  components: {
+    vHeader
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
